1bb5a10aa51768e8aa6a26a9318db906fa9ff97c
Hugo Oliveira <holiveira@ciandt.com>
2021-01-25 08:15:14 -0300

    Updating login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import { Container } from './styles';
 
-const Login: React.FC = () => <Container>Ola</Container>;
+const logoIheart =
+  'https://images.squarespace-cdn.com/content/v1/54becebee4b05d09416fe7e4/1475621488473-GG13ZAH35UK8B4AQ7IG6/ke17ZwdGBToddI8pDm48kPatQlumu_Sh_yEKNNtje2NZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7aXK0t8ahyzoOLFEHArbPTIvsJ8ZBSuac1iGVlJVssK0Dume1Xhlc5QUWr24itNJvQ/iHM_primary_Color.jpg?format=300w';
+
+const Login: React.FC = () => (
+  <Container>
+    <form>
+      <img src={logoIheart} alt="iheart_logo" />
+      <label htmlFor="uname">
+        <input type="text" placeholder="Username" name="uname" required />
+      </label>
+
+      <label htmlFor="psw">
+        <input type="password" placeholder="Password" name="psw" required />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  </Container>
+);
 
 export default Login;
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -1,7 +1,29 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  position: fixed; /* or absolute */
-  top: 50%;
-  left: 50%;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  & input[type='text'],
+  input[type='password'] {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+  }
+  & button {
+    background-color: #4caf50;
+    color: white;
+    padding: 14px 20px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+  }
 `;
