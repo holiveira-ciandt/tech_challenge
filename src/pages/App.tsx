af1bb67b590d808201b59780b06bdb7a09cc7589
Hugo Oliveira <holiveira@ciandt.com>
2021-01-25 11:41:15 -0300

    Add header

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyles from 'assets/css/global';
 import Routes from 'routes';
 import Theme from 'components/theme';
+import Header from 'pages/header';
 
 const App: React.FC = () => (
   <BrowserRouter>
     <Theme>
       <GlobalStyles />
+      <Header />
       <Routes />
     </Theme>
   </BrowserRouter>
--- src/pages/header/styles.ts
+++ src/pages/header/styles.ts
@@ -1,11 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  //display: flex;
   padding-bottom: 20px;
-  position: fixed;
-  width: 100%;
-  margin: 0 auto;
+  width: 100vw;
   top: 0;
   & img {
     display: block;
