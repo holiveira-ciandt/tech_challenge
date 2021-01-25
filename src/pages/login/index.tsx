f625c60544969ff62bc3f370130f2d1d4f1f5e09
Hugo Oliveira <holiveira@ciandt.com>
2021-01-25 08:25:28 -0300

    Redirect to songs table

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import { Container } from './styles';
 
 const logoIheart =
   'https://images.squarespace-cdn.com/content/v1/54becebee4b05d09416fe7e4/1475621488473-GG13ZAH35UK8B4AQ7IG6/ke17ZwdGBToddI8pDm48kPatQlumu_Sh_yEKNNtje2NZw-zPPgdn4jUwVcJE1ZvWhcwhEtWJXoshNdA9f1qD7aXK0t8ahyzoOLFEHArbPTIvsJ8ZBSuac1iGVlJVssK0Dume1Xhlc5QUWr24itNJvQ/iHM_primary_Color.jpg?format=300w';
 
 const Login: React.FC = () => {
+  const history = useHistory();
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     console.log('Event', event);
+    history.push('/songs');
   };
   return (
     <Container>
